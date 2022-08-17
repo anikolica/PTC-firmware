98bbf1bd69cffd61b51abf59716114f1f87000d1
anikolic <13853168+anikolica@users.noreply.github.com>
2022-08-17 14:58:30 -0400

    Change build script to 5EV model

--- reference_design/scripts/settings.tcl
+++ reference_design/scripts/settings.tcl
@@ -34,7 +34,7 @@
 
 # ----------------------------------------------------------------------------------
 # Modify this variable to select your module
-if {![info exists module_name]} {set module_name ME-XU5-2CG-1E-D10H}
+if {![info exists module_name]} {set module_name ME-XU5-5EV-2I-D12E}
 if {![info exists baseboard]}   {set baseboard PE1}
 # ----------------------------------------------------------------------------------
 
