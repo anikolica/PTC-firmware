0b44a8eb187ffa22766df676431dce6697b99d83
Eliot Keener <eliotkeener@gmail.com>
2025-07-09 12:34:02 -0400

    Implementation to set duty cycle value when reset is asserted

--- Verilog_Modules/PWM_inc_dec/PWM_main.v
+++ Verilog_Modules/PWM_inc_dec/PWM_main.v
@@ -1,20 +1,17 @@
-`include "counter.v"
-
 module PWM_main(
 	input clk,
+	input rst,
 	input duty_inc,
 	input duty_dec,
+	input [2:0]  duty,
 	output reg PWM_out //Output pin for the modified signal
 );
-	wire [3:0] counter; //The counter that is compared to the duty cycle
-	reg [3:0] duty_cycle = 4'b1000; //Duty cycle declaration
-	
-	//Instantiate a counter module
-	counter counter_inst(
-		.clk(clk),
-		.counter(counter)
-	);
+	reg [2:0] counter; //The counter that is compared to the duty cycle
+	reg [2:0] duty_cycle;
 
+	//Instantiate a counter module
+	always @(posedge clk)
+		counter <= counter + 1'b1;
 	
 	//Comparator for duty cycle
 	always @(posedge clk)
@@ -25,10 +22,12 @@ module PWM_main(
 
 	//If either duty_inc or duty_dec, change the cycle by approximately 10%
 	always @(posedge clk)
-		if(duty_inc==1 && duty_cycle < 13)
-			duty_cycle = duty_cycle + 2;
-		else if(duty_dec && duty_cycle >= 2)
-			duty_cycle = duty_cycle - 2;
-		else
-			duty_cycle = duty_cycle;
+		if (rst)
+			duty_cycle <= duty;
+		else begin
+			if(duty_inc==1 && duty_cycle < 7)
+				duty_cycle <= duty_cycle + 1;
+			else if(duty_dec && duty_cycle >= 1)
+				duty_cycle <= duty_cycle - 1;
+		end
 endmodule
--- Verilog_Modules/PWM_inc_dec/PWM_tb.v
+++ Verilog_Modules/PWM_inc_dec/PWM_tb.v
@@ -1,24 +1,27 @@
-`timescale 10ns / 1ns
+`timescale 1ns / 1ps
 `include "PWM_main.v"
 
 
 module PWM_tb;
 	reg clk;
-	reg reset;
+	reg rst;
 	reg duty_inc;
 	reg duty_dec;
+	reg [2:0] duty_cycle;
 	wire PWM_out;
 
 
-	parameter HALF_CYCLE = 5; //Define the half period of the clock cycle
-	parameter CYCLE = 10; //Full clock cycle
+	parameter HALF_CYCLE = 50; //Define the half period of the clock cycle
+	parameter CYCLE = 100; //Full clock cycle
 	parameter PAUSE = CYCLE * 100; //A prolonged pause to observe changes
 
 	//Instantiate the PWM generator
 	PWM_main PWM_inst(
 		.clk(clk),
+		.rst(rst),
 		.duty_inc(duty_inc),
 		.duty_dec(duty_dec),
+		.duty(duty_cycle),
 		.PWM_out(PWM_out)
 	);
 
@@ -31,7 +34,11 @@ module PWM_tb;
 		$dumpfile("PWM.vcd");
 		$dumpvars(0, clk, PWM_out, duty_inc, duty_dec, PWM_inst.duty_cycle);
 		clk=1'b0;
-		PWM_inst.counter_inst.counter = 8'b0;
+		duty_cycle = 3'b100;	
+		PWM_inst.counter = 3'b0;
+		rst = 1'b1;
+		#PAUSE
+		rst = 1'b0;
 
 		duty_inc = 0;
 		duty_dec = 0;
@@ -44,11 +51,11 @@ module PWM_tb;
 		#CYCLE
 		duty_inc = 0;
 		#PAUSE
-                duty_inc = 1;
+        duty_inc = 1;
 		#CYCLE
 		duty_inc = 0;
 		#PAUSE
-	        duty_dec = 1;
+	    duty_dec = 1;
 		#CYCLE
 		duty_dec = 0;
 		#PAUSE
