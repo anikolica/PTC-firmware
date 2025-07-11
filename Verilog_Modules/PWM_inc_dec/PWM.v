d2f8199735ed7d77fcefd8ffd09a3fc28cdd75ec
HEP <eliotkeener@gmail.com>
2025-07-11 13:01:11 -0400

    Vivado approved format for PWM module with clock divider

--- Verilog_Modules/PWM_inc_dec/PWM.v
+++ Verilog_Modules/PWM_inc_dec/PWM.v
@@ -24,51 +24,51 @@ module PWM(
 		.clk_div(clk_div)
 	);
 
-	//Reset will initialize everything to zero and a duty cycle of 50%
-	always @(posedge rst)
-	begin
-		counter <= 0;
-		duty_prev <= 0;
-		duty_cycle <= 3'b100;
-		PWM_out <= 0;
-	end
-
-	//Check to see if the duty input changed
-	always @(posedge clk_div)
-	begin
-		duty_prev <= duty; //These lines run in parallel
-		duty_changed <= (duty_prev != duty);
-	end
-
-	//Block for counter and PWM 
-	always @(posedge clk_div)
+	//Main Logic Block
+	always @(posedge clk_div or posedge rst)
 		if(en) begin
-			counter <= counter + 1'b1;	
-			//Comparator for duty cycle
-				if(duty_cycle > counter)
-					PWM_out <= 1'b1;
-				else
-					PWM_out <= 1'b0;
-			end
-		else begin 
+		 //On reset, set key values to zero and duty cycle to 50%
+		    if(rst) begin
+		      counter <= 0;
+		      duty_prev <= 0;
+		      duty_cycle <= 3'b100;
+		      PWM_out <= 0;
+		    end
+		    
+		  //Run through the PWM process while reset is not asserted
+		    else begin
+		      //Counter for PWM
+		          counter <= counter + 1'b1;	
+			 //Comparator for duty cycle
+				    if(duty_cycle > counter)
+					   PWM_out <= 1'b1;
+				    else
+					   PWM_out <= 1'b0;
+					   
+		  //Lines to check if the duty cycle changed
+			duty_prev <= duty; //These lines run in parallel
+		    duty_changed <= (duty_prev != duty);
+		    
+		  //Block to modify the duty cycle
+			 //Assigns the duty cycle if it was changed via control bits
+			 if(duty_changed) begin
+				    duty_cycle <= duty;
+				    duty_changed <= 1'b0;
+			 end
+			 
+			 //Changes the duty cycle by 12.5% if duty_inc or duty_dec are asserted
+			 else begin
+				    if(duty_inc==1 && duty_cycle < 7)
+					   duty_cycle <= duty_cycle + 1;
+				    else if(duty_dec && duty_cycle >= 1)
+					   duty_cycle <= duty_cycle - 1;
+				    end
+		      end
+	   end
+	   
+	   //When enable is not asserted, the output is 0
+	   else begin 
 			PWM_out <= 1'b0;
 
-		end
-
-	//If either duty_inc or duty_dec, change the cycle by approximately 10%
-	always @(posedge clk_div)
-		if(en) begin
-			//Assigns the duty cycle if it was changed via control bits
-			if(duty_changed) begin
-				duty_cycle <= duty;
-				duty_changed <= 1'b0;
-			end
-			//Changes the duty cycle if duty_inc or duty_dec are asserted
-			else begin
-				if(duty_inc==1 && duty_cycle < 7)
-					duty_cycle <= duty_cycle + 1;
-				else if(duty_dec && duty_cycle >= 1)
-					duty_cycle <= duty_cycle - 1;
-				end
 		end
 endmodule
