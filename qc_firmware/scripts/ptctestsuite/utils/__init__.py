5ebbd6f137a230e554bfd294b2a4644068439fdf
David Drobner <david@drobner.ca>
2026-01-27 16:12:24 -0500

    Add WIP manual test base class

--- qc_firmware/scripts/ptctestsuite/utils/__init__.py
+++ qc_firmware/scripts/ptctestsuite/utils/__init__.py
@@ -1,3 +1,4 @@
 from ptctestsuite.utils.qc_record import qc_record
 from ptctestsuite.utils.qc_result import qc_result
 from ptctestsuite.utils.test_base import test_base
+from ptctestsuite.utils.manual_test import manual_test
---
+++ qc_firmware/scripts/ptctestsuite/utils/manual_test.py
@@ -0,0 +1,38 @@
+from ptctestsuite import test_base
+from ptctestsuite import qc_result
+
+class manual_test(test_base):
+
+    def __init__(self, test_name: str):
+        self._test_name = test_name 
+        # overriding this for the dict of pretty print names
+        self.__class__.__name__ = test_name
+        # setting this as error by default
+        self._test_status = qc_result.ERROR
+        self._test_value = None
+    
+    @property
+    def test_name(self):
+        return self._test_name
+
+    @test_name.setter
+    def test_name(self, value: str):
+        try:
+            self._test_name = str(value)
+            self.__class__.__name__ = str(value)
+        except:
+            raise ValueError("Cannot cast passed value to a string.")
+
+    @property
+    def test_status(self):
+        return self._test_status
+
+    # simple getter and setter method for this one. 
+    # expectation is that the tester will indicate pass or fail
+    # TODO review if that is the preferred behaviour
+    @test_status.setter
+    def test_status(self, status: qc_result):
+        self._test_status = status
+
+    def run_test(self) -> qc_result:
+        return self._test_status
