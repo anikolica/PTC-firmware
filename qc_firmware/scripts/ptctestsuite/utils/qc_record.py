019513aac805a8defee2217ca1d6568055ad8e4f
David Drobner <david@drobner.ca>
2026-01-28 16:09:52 -0500

    Add datasheet generator function

--- qc_firmware/scripts/ptctestsuite/utils/qc_record.py
+++ qc_firmware/scripts/ptctestsuite/utils/qc_record.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
 import yaml
+import time
 
 # this may be unnecessary. Need to check how the HWDB REST API works
 @dataclass
@@ -12,11 +13,15 @@ class hwdb_record:
 
 @dataclass
 class qc_record:
-    test_status: dict
     serial_number: str
     tester_name: str
+    test_status: dict
+    tester_notes: str
     test_location: str = "University of Pennsylvania"
     test_datetime: datetime = datetime.now()
 
     def gen_hwdb_datasheet(self):
-        return yaml.dump(asdict(self))
+        dd = asdict(self)
+        dd['test_date'] = dd['test_datetime'].strftime("%Y-%m-%d")
+        dd['test_time'] = f"{dd['test_datetime'].strftime('%H:%M:%S')} {time.tzname[0]}"
+        return yaml.dump(dd, sort_keys=False)
