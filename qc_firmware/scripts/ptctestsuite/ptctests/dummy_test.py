368f16a385f40f76df9eea3e49288d60838e99b5
David Drobner <david@drobner.ca>
2026-01-29 17:21:01 -0500

    Add dummy test for development

---
+++ qc_firmware/scripts/ptctestsuite/ptctests/dummy_test.py
@@ -0,0 +1,18 @@
+from ptctestsuite import test_base
+from ptctestsuite import qc_result
+from loguru import logger as lg
+
+class dummy_test(test_base):
+    def __init__(self, name: str):
+        self._name = name 
+
+    def run_test(self):
+        lg.info(f"Running dummy test named {self._name}")
+        return qc_result.PASS
+
+    def test_init(self) -> bool:
+        return super().test_init()
+    
+    def test_end(self) -> bool:
+        return super().test_end()
+        
