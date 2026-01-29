d073335f77cba59cedede1932e79e64bbf9ed843
David Drobner <david@drobner.ca>
2026-01-29 17:20:14 -0500

    Add imports for ptc tests

---
+++ qc_firmware/scripts/ptctestsuite/ptctests/__init__.py
@@ -0,0 +1,4 @@
+from ptctestsuite.ptctests.dummy_test import dummy_test
+from ptctestsuite.ptctests.eeprom import eeprom 
+from ptctestsuite.ptctests.gbe_test import gbe_test
+from ptctestsuite.ptctests.temperature import temperature_test
