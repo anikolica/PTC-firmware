6e1f8c4e7b1343e136a183a3dbdf45e2435b3678
David Drobner <david@drobner.ca>
2026-01-30 15:15:59 -0500

    Clean up imports

--- qc_firmware/scripts/ptctestsuite/__init__.py
+++ qc_firmware/scripts/ptctestsuite/__init__.py
@@ -1,2 +1,2 @@
-from .utils.qc_result import qc_result
-from .utils.test_base import test_base
+from ptctestsuite.utils.qc_result import qc_result
+from ptctestsuite.utils.test_base import test_base
