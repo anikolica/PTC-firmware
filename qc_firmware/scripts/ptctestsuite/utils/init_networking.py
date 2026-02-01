4acd78f1fa74d146b0d4d704bbb8f4e37589b5cb
David Drobner <david@drobner.ca>
2026-02-01 18:33:20 -0500

    Add script to initialize networking

---
+++ qc_firmware/scripts/ptctestsuite/utils/init_networking.py
@@ -0,0 +1,41 @@
+import asyncio
+import serial_asyncio
+import serial
+
+async def init_ptc(serial_port="/dev/ttyUSB0", baudrate=12800, timeout=1, ip_address = "192.168.20.112") -> bool:
+    try:
+        line_end = r'\r\n'
+        # in case we actually need UTF-8
+        encoding = 'ascii'
+        # sleep time between commands
+        sleep_time = 2
+        
+        # establish a serial connection
+        reader, writer = await serial_asyncio.open_serial_connection(url=serial_port, baudrate=baudrate, timeout=timeout)
+
+        # log into the ptc
+        writer.write(f"ptc{line_end}".encode(encoding))
+        await writer.drain()
+        await asyncio.sleep(sleep_time)
+
+        # assign IP
+        iface_str = f"ifconfig eth1 {ip_address} netmask 255.255.255.0{line_end}"
+        writer.write(iface_str.encode(encoding))
+        await writer.drain()
+        await asyncio.sleep(sleep_time)
+
+        # set correct bit to enable GbE
+        writer.write(f"poke 0xff0c0004 0x092e2c4a{line_end}".encode('ascii'))
+        await writer.drain()
+        await asyncio.sleep(sleep_time)
+
+        # close the writer
+        writer.close()
+        await writer.wait_closed()
+
+        # successful init
+        return True
+    except:
+        # something went wrong
+        return False
+    
