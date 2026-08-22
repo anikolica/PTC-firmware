1683d22a25676169e526e60488fac245b6917105
David Drobner <david@drobner.ca>
2026-08-22 11:04:51 -0400

    Fix setup-yocto

--- reference_design/yocto-workspace/setup-yocto.sh
+++ reference_design/yocto-workspace/setup-yocto.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Clone upstream layers if they don't exist
-git clone -b scarthgap git://git.yoctoproject.org/poky
+git clone -b scarthgap https://git.yoctoproject.org/poky
 git clone -b scarthgap https://github.com/openembedded/meta-openembedded
-git clone -b scarthgap git://git.yoctoproject.org/meta-arm
+git clone -b scarthgap https://git.yoctoproject.org/meta-arm
 git clone -b scarthgap https://git.yoctoproject.org/meta-virtualization
-git clone -b rel-v2025.2 https://github.com/Xilinx/meta-xilinx
-git clone -b rel-v2025.2 https://github.com/Xilinx/meta-xilinx-tools
+git clone -b rel-v2025.1 https://github.com/Xilinx/meta-xilinx
+git clone -b rel-v2025.1 https://github.com/Xilinx/meta-xilinx-tools
 
 # Initialize build environment
 source poky/oe-init-build-env build
